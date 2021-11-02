db71efe365f18c63beacc778751f06b660298439
DenTis2 <74709635+DenTis2@users.noreply.github.com>
2021-11-02 17:09:54 +0700

    Add files via upload

--- CV/binarytest.cpp
+++ CV/binarytest.cpp
@@ -1,90 +1,120 @@
-﻿/*
- *  Пример захвата с камеры, бинаризации и вывода в окно для OpenCV версии 2  
- *  Заменил IplImage на Mat и команды, которые работают с Mat
- *  Выход из программы по нажатию на кнопку ESC
- 
- Компилировать в Linux:
-
-g++ `pkg-config --cflags opencv` -o binarytest binarytest.cpp `pkg-config --libs opencv`
- 
- */
-
-#include <cv.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <opencv2/opencv.hpp>
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include <highgui.h>
+#include <opencv2/highgui.hpp>
 #include <stdlib.h>
 #include <stdio.h>
+#include <vector>
 
-using namespace cv;
 
 int main() {
-	VideoCapture cap(0);
+    cv::VideoCapture cap(0);
     //cap.set(CV_CAP_PROP_FRAME_WIDTH,320);
     //cap.set(CV_CAP_PROP_FRAME_HEIGHT,240);
-    cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
-    cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
-    Mat frame; 
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    cv::Mat frame;
     cap >> frame;
     cv::imshow("Bin", frame);
-    
-    int hMin = 0;
-    int hMax = 0;
 
-    int sMin = 0;
-    int sMax = 0;
+    int hMin = 72;
+    int hMax = 92;
 
-    int vMin = 0;
-    int vMax = 0;
+    int sMin = 110;
+    int sMax = 255;
+
+    int vMin = 48;
+    int vMax = 255;
+
+    int cannyThreshold = 0;
+    int eccentricityThreshold = 10;
+    int areaThreshold = 10;
 
     int chMax = 255;
-    
-    
-    char hMinName[50];
-    char hMaxName[50];
-    
-    char sMinName[50];
-    char sMaxName[50];
-    
-    char vMinName[50];
-    char vMaxName[50];
-    
-    std::sprintf(hMinName, "H min %d", hMin);
-    std::sprintf(hMaxName, "H max %d", hMax);
-    
-    std::sprintf(sMinName, "S min %d", sMin);
-    std::sprintf(sMaxName, "S max %d", sMax);
-    
-    std::sprintf(vMinName, "V min %d", vMin);
-    std::sprintf(vMaxName, "V max %d", vMax);
 
-    cv::createTrackbar(hMinName, "Bin", &hMin, chMax);
-    cv::createTrackbar(hMaxName, "Bin", &hMax, chMax);
+    cv::createTrackbar("H min", "Bin", &hMin, chMax);
+    cv::createTrackbar("H max", "Bin", &hMax, chMax);
 
-    cv::createTrackbar(sMinName, "Bin", &sMin, chMax);
-    cv::createTrackbar(sMaxName, "Bin", &sMax, chMax);
+    cv::createTrackbar("S min", "Bin", &sMin, chMax);
+    cv::createTrackbar("S max", "Bin", &sMax, chMax);
 
-    cv::createTrackbar(vMinName, "Bin", &vMin, chMax);
-    cv::createTrackbar(vMaxName, "Bin", &vMax, chMax);
+    cv::createTrackbar("V min", "Bin", &vMin, chMax);
+    cv::createTrackbar("V max", "Bin", &vMax, chMax);
 
-	cv::Mat image;
-	
+    cv::createTrackbar("Canny", "Bin", &cannyThreshold, 100);
+    cv::createTrackbar("Eccentricity", "Bin", &eccentricityThreshold, 100);
+    cv::createTrackbar("Area", "Bin", &areaThreshold, 100);
+
+    cv::Mat image, detectedEdges;
+
+    cv::RNG rng(12345);
+
     while (true) {
-
-        //cv::Mat image = mur.getCameraOneFrame();
         cap >> image;
         cv::imshow("Image", image);
-        cv::cvtColor(image, image, CV_BGR2HSV);
+        cv::cvtColor(image, image, cv::COLOR_BGR2HSV);
         cv::Scalar lower(hMin, sMin, vMin);
         cv::Scalar upper(hMax, sMax, vMax);
         cv::inRange(image, lower, upper, image);
-        cv::imshow("Bin", image);
-        char c = cv::waitKey(10);
-        if (c == 27) { // если нажата ESC - выходим
-			break;
-	}
+
+        cv::OutputArray edges();
+        cv::blur(image, detectedEdges, cv::Size(5, 5));
+        cv::Canny(detectedEdges, detectedEdges, cannyThreshold, cannyThreshold * 3.0);
+
+        cv::Mat result(cv::Mat::zeros(image.size(), CV_8UC3));
+
+        std::vector<std::vector<cv::Point> > contours;
+        cv::findContours(detectedEdges, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+        auto areaThresholdInterpolated = (int)((double)areaThreshold / 100.0) * image.total();
+
+        for (auto contour : contours) {
+            if (contour.size() < 6) continue;
+
+            cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+
+            auto ellipse = cv::fitEllipse(contour);
+            auto width = ellipse.boundingRect().width;
+            auto height = ellipse.boundingRect().height;
+
+            auto majorAxisLength = std::max(width, height);
+            auto minorAxisLength = std::min(width, height);
+
+            auto eccentricity = std::sqrt(1 - std::pow(minorAxisLength / majorAxisLength, 2));
+            if (eccentricity > (double)eccentricityThreshold / 100.0) continue;
+            if (cv::contourArea(contour) < areaThresholdInterpolated) continue;
+
+            cv::ellipse(result, ellipse, color, 2);
+        }
+
+        cv::imshow("Bin", result);
+        cv::imshow("Canny", detectedEdges);
+
+        if (cv::waitKey(10) == 27) { // ���� ������ ESC - �������
+            break;
+        }
     }
-    cvDestroyAllWindows();
+
+    /*
+        edges = cv2.Canny(img, lower, upper)
+        cv2.imshow('Edges', edges)
+        _, contours, _ = cv2.findContours(edged, cv2.RETR_TREE, 1)
+        rep = cv2.drawContours(img1, contours, -1, (0, 255, 0), 3)
+        cv2.imshow(Contours',rep)
+
+    //for (auto countour : cnt)
+    cnt = contours
+    for i in range(0, len(cnt)):
+        ellipse = cv2.fitEllipse(cnt[i])
+        (center,axes,orientation) =ellipse
+        majoraxis_length = max(axes)
+        minoraxis_length = min(axes)
+        eccentricity=(np.sqrt(1-(minoraxis_length/majoraxis_length)**2))
+        cv2.ellipse(img2,ellipse,(0,0,255),2)
+
+        */
+    cv::destroyAllWindows();
 }
